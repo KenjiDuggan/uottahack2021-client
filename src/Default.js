ad71982c7970e24769f37782e8e9a69dc9b90071
KenjiDuggan <kenjiduggan@gmail.com>
2021-02-06 16:00:32 -0500

    feed page update, going to finish it now make it look better and stuff

--- src/Default.js
+++ src/Default.js
@@ -1,10 +1,31 @@
 
 import React, { useEffect, useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import NewsCard from "./components/NewsCard/NewsCard";
 import SampleComponent from "./SampleComponent";
+import Grid from '@material-ui/core/Grid';
 import "./Default.css";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+      backgroundColor: theme.palette.background.paper,
+     
+    },
+    paper: {
+      padding: theme.spacing(3),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      margin: '10px'
+    },
+  }));
  
 const Default = (props) => {
+    const classes = useStyles();
     const [articles, setArticles] = useState(props.news);
 
     useEffect(() => {
@@ -12,15 +33,17 @@ const Default = (props) => {
     }, [props.news]);
 
   return (
-    <div className="Default">
+    <div >
         <h1>COVID-19 Updates</h1>
         <div>Ontario</div>
-        <header className="App-header">
-        { articles.length > 0 && articles.map((article, i) => (
-            <NewsCard key={article} article={article} key={i} />
-        ))}
-        <SampleComponent color="blue" />
-        </header>
+        <Grid spacing={2}  className={classes.root}>
+           
+            { articles.length > 0 && articles.map((article, i) => (
+                <NewsCard key={article} article={article} key={i} />
+            ))}
+            <SampleComponent color="blue" />
+         
+        </Grid>
     </div>
   );
 }
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,13 +1,55 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const NewsCard = ({ article }) => {
+  const classes = useStyles();
   console.log(article);
   const { author, content, description, publishedAt, source, title, url, urlToImage } = article;
   return (
-    <Box border={1} borderColor="white">
-      { author }
-    </Box>
+    <Grid item xs={4}>
+        <Card >
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt="Contemplative Reptile"
+              height="140"
+              image={urlToImage}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="h5">
+                { title }
+              </Typography>
+              
+              <Typography variant="body2" color="textSecondary" component="p">
+                { description }
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <a href="source">source</a>
+            <Button size="small" color="primary"> //onClick event to specific image page
+              Learn More
+            </Button>
+          </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
