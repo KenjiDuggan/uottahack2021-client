04885e4a6c234b82ea0aa7e158ce2de2f5ce042d
KenjiDuggan <kenjiduggan@gmail.com>
2021-02-07 02:44:10 -0500

    fake qr

--- src/NewsPage.js
+++ src/NewsPage.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import history from './history'
 import { useParams } from 'react-router'
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import './NewsPage.css';
 
 const NewsPage = () => {
@@ -29,15 +30,23 @@ const NewsPage = () => {
 
   return (
     <Container maxWidth="md">
-        <section>
-            <h1>{post.title}</h1>
-            <img alt="blog background" src={post.urlToImage} />
-            <p>{post.content}</p>
-            <div className="bigButton" onClick={routeChange}>
-                <h1 className="bigButtonText">Translate Now</h1>
-                <img alt="translate logo" src={process.env.PUBLIC_URL + '/images/translate.svg'} className="globeImage icon-comment iccoo" />
-            </div>
-        </section>
+        <h1>{post.title}</h1>
+        <img alt="blog background" src={post.urlToImage} className="postImage" />
+        <h3>{post.content}</h3>
+        <Grid container spacing={3}>
+            <Grid item xs={6}>
+                 <div className="qrcode">
+                    <h1 className="bigButtonText">Speak To Me</h1>
+                    <img src={process.env.PUBLIC_URL + '/images/qr_example.jpg'} className="qrcodeImage" />
+                 </div> 
+            </Grid>
+            <Grid item xs={6}>
+                <div className="bigButton" onClick={routeChange}>
+                    <h1 className="bigButtonText">Translate Now</h1>
+                    <img alt="translate logo" src={process.env.PUBLIC_URL + '/images/translate.svg'} className="globeImage icon-comment iccoo" />
+                </div>
+            </Grid> 
+        </Grid>
     </Container>
   )
 }
